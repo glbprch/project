9188f28b419bb98781ed7f618474c133165f5fae
glbprch <121551889+glbprch@users.noreply.github.com>
2023-01-14 23:15:55 +0600

    Update program.py

--- program.py
+++ program.py
@@ -8,6 +8,9 @@ pygame.font.init()
 FONT = pygame.font.SysFont('calibry', 50)
 RES = WIDTH, HEIGHT = 1600, 900
 NUM_STARS = 101
+CENTER = vec2(WIDTH // 2, HEIGHT // 2)
+COLORS = "red green blue orrange purple cyan".split
+Z_DISTANCE = 40
 
 
 class Menu:
@@ -41,13 +44,28 @@ class Menu:
 
 class Star:
     def __init__(self, app):
-        pass
-
+        self.screen = app.screen
+        self.pos3d = self.get_pos3d()
+        self.vel = random.uniform(0.05, 0,25)
+        self.color = random.choice(COLORS)
+        self.screen_pos = vec2(0, 0)
+        self.size = 10
+        
+    def get_pos3d(self, app):
+        angle = random.uniform(0, 2 * math.pi)
+        radius = random.randrange(HEIGHT)
+        x = radius * math.sin(angle)
+        y = radius * math.cos(angle)
+        return vec3(x, y, Z_DISTANCE)
+        
     def update(self):
-        pass
+        self.pos3d.z -= self.vel
+        self.pos3d = self.get_pos3d() if self.pos3d.z < 1 else self.pos3d
+        
+        self.screen_pos = vec2(self.pos3d.x, self.pos3d.y) / self.pos3d.z + CENTER
 
     def draw(self):
-        pass
+        pg.draw.rect(self.screen, self.color, (*self.screen_pos, self.size, self.size))
 
 
 class Starfild:
